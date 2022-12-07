86f4a68d59ae04e06757b97be084a1d91f4ed29b
superdev <superdev0241002@gmail.com>
2022-12-08 01:49:53 +0900

    Add feat: connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/testdb')], //@ Connect Database
   controllers: [AppController],
   providers: [AppService],
 })
